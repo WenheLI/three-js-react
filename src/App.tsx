9bec4efc942024cd0813a2e4a8dc0717e2987e25
WenheLI <wl1508@nyu.edu>
2021-11-18 15:04:22 -0500

    update lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import App3D from './3D/index';
 
 function App() {
-  const [camera, setCamera] = useState<THREE.PerspectiveCamera>();
   return (
     <div id='master-container'>
       <App3D />
